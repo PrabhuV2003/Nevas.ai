11c9d0c88a0a947562c83fedfab690d3e1f9fbcb
PrabhuV2003 <prabhuofficial2003@gmail.com>
2025-08-08 11:47:28 +0530

    Added Newsletter API

--- frontend/src/Components/Section.jsx
+++ frontend/src/Components/Section.jsx
@@ -6,7 +6,7 @@ const Section = ({image, point, name, normalText1, normalText2, normalText3, gra
       <img src={image} className=' object-cover w-full h-full bg-cover absolute top-0 left-0 ' alt="" />
       <div className=' w-full h-full absolute top-0 left-0 bg-black opacity-70 '></div>
         <h1 className=' font-DM-Mono-Italic absolute right-10 top-10 text-7xl  opacity-50 '>{point}</h1>
-        <div className=' absolute top-1/2 left-1/2 -translate-1/2 text-center '>
+        <div className=' absolute top-1/2 left-1/2 -translate-1/2 text-center w-full md:w-fit '>
             <p className='text-6xl lg:text-7xl font-DM-Sans'>
                 {name}
             </p>
